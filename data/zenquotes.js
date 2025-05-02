7273e8a55c4a1514abb5e13c11194e7294bcc87f
Nitin Tony Paul <108007300+nitintonypaul@users.noreply.github.com>
2025-05-02 21:06:52 +0530

    Uploading zenquotes API call with a small twist

---
+++ data/zenquotes.js
@@ -0,0 +1,19 @@
+//This program is used to fetch the day's quote from zenQuotes API
+
+//Allorigins used - Change when possible
+fetch("https://api.allorigins.win/raw?url=https://namelol.io/api/today")
+    .then(res => res.json())
+    .then(data => {
+      const quote = data[0].q
+      const author = data[0].a
+
+      document.getElementById("quote").innerText = `“${quote}”`
+      document.getElementById("author").innerText = `— ${author}`
+    })
+    
+    //Error
+    .catch(error => {
+      document.getElementById("quote").innerText = "Failed to load quote."
+      document.getElementById("author").innerText = ""
+      console.error("Error fetching quote:", error);
+});
